6b9da5c356471de4ce0451e61b65b2c2dae06ab7
krishna <krishna@biz4group.com>
2023-10-17 20:44:12 +0530

    update code

--- src/components/coding/ProblemList.tsx
+++ src/components/coding/ProblemList.tsx
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from "react";
-import Problems from "./problems.json";
-import Categorys from "./category.json";
 import axios from "axios";
-
-interface IProblems {
-  id: number;
-  title: string;
-  category_id: number;
-  code_id: number;
-}
 
 const ProblemList: React.FC = () => {
   const [listt, setListt] = useState<any[]>();
+  const [loading, setLoading] = useState(false);
   /**
    *  GROUP BY THE PROBLEMS BY CATEGORY ID
    */
 
   const getProblemList = async () => {
-    const { data } = await axios.get(
-      `http://localhost:3005/api/coding/problems`
-    );
-
-    if (data && data.success) {
-      setListt(data.data);
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `http://localhost:3005/api/coding/problems`
+      );
+      if (data && data.success) {
+        setListt(data.data);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
     }
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -32,34 +30,36 @@ const ProblemList: React.FC = () => {
 
   return (
     <div className="row container mt-5 mx-auto">
-      {listt &&
-        listt.length > 0 &&
-        listt.map((it, index) => {
-          return (
-            <div className="col-md-4 ">
-              <ul key={index} className="list-group">
-                <li
-                  key={it.category}
-                  className="list-group-item active"
-                  aria-current="true"
-                >
-                  {it?.category}
-                </li>
+      {listt && listt.length > 0
+        ? listt.map((it, index) => {
+            return (
+              <div className="col-md-4 ">
+                <ul key={index} className="list-group">
+                  <li
+                    key={it.category}
+                    className="list-group-item active"
+                    aria-current="true"
+                  >
+                    {it?.category}
+                  </li>
 
-                {it &&
-                  it.problems &&
-                  it.problems.length > 0 &&
-                  it.problems.map((item: { title: string }, ind: number) => {
-                    return (
-                      <li key={ind} className="list-group-item shadow">
-                        {item?.title}
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
-          );
-        })}
+                  {it &&
+                    it.problems &&
+                    it.problems.length > 0 &&
+                    it.problems.map((item: { title: string }, ind: number) => {
+                      return (
+                        <li key={ind} className="list-group-item shadow">
+                          {item?.title}
+                        </li>
+                      );
+                    })}
+                </ul>
+              </div>
+            );
+          })
+        : loading
+        ? "Loading..."
+        : "Data Not Found"}
     </div>
   );
 };
