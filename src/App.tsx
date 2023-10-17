c703037e53bc1d1e45cdf24ba8e2dcd9c549ad53
krishna <krishna@biz4group.com>
2023-10-17 20:16:13 +0530

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,17 @@ import ProblemList from "./components/coding/ProblemList";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Endpoints } from "./constant";
-import Test from "./components/Test";
 
 const App: React.FC = () => {
   return (
     <div>
-      {/* <Router>
+      <Router>
         <Navbar />
         <Routes>
           <Route path={Endpoints.PROBLESMS} element={<ProblemList />} />
           <Route path={Endpoints.COMPILER} element={<CodeEditor />} />
         </Routes>
-      </Router> */}
-      <Test />
+      </Router>
     </div>
   );
 };
