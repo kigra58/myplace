56670d05d22ee05f5e7d4199ea767161442d9fdb
krishna <krishna@biz4group.com>
2023-10-22 20:21:50 +0530

    update

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,34 +1,27 @@
 import React from "react";
+import { Toast } from "react-bootstrap";
 
 interface IProps {
-  message:string;
-  success:boolean
+  message: string;
+  success: boolean;
 }
 
-const Toast: React.FC<IProps> = ({ message,success}) => {
-console.log("==============oooooooooo",message,success);
+const Toast1: React.FC<IProps> = ({ message, success }) => {
+  // console.log("==============oooooooooo", message, success);
   if (message !== "") {
     return (
-      <div
-        className={`toast align-items-center text-white ${success ? "bg-primary":"bg-dangers"} border-0 shadow`}
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-      >
-        <div className="d-flex">
-          <div className="toast-body">{message}</div>
-          <button
-            type="button"
-            className="btn-close btn-close-white me-2 m-auto"
-            data-bs-dismiss="toast"
-            aria-label="Close"
-          ></button>
-        </div>
-      </div>
+      <Toast delay={5000} bg={`${success ? "primary" : "danger"} text-white`}>
+        {/* <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
+          <small>11 mins ago</small>
+        </Toast.Header> */}
+        <Toast.Body>{message}</Toast.Body>
+      </Toast>
     );
   } else {
     return null;
   }
 };
 
-export default Toast;
+export default Toast1;
